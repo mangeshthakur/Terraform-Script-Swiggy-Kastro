ddf8413c8042ca591eded95e700c95d8239c2ec5
Mangesh Balkrushna Thakur <mangesh.thakur44@gmail.com>
2025-07-13 15:08:26 +0530

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -31,7 +31,7 @@ resource "aws_security_group" "Project-SG" {
 
 
 resource "aws_instance" "web" {
-  ami                    = "ami-001f2488b35ca8aad"
+  ami                    = "ami-010876b9ddd38475e"
   instance_type          = "t2.large"
   key_name               = "Kastro"
   vpc_security_group_ids = [aws_security_group.Project-SG.id]
